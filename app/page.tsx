682ab1bf468b20007d5d9ace1a3f8a3eead75a31
AbhimantraPNW <abhimantrapnw04@gmail.com>
2024-03-23 05:44:39 +0700

    tes image

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
     <Hero />
-    <CompanyProfile />
+    {/* <CompanyProfile /> */}
     <Products />
     <Testimonial />
     <Footer />
--- components/CompanyProfile.tsx
+++ components/CompanyProfile.tsx
@@ -15,7 +15,7 @@ const Company = ({
 }: CompProps) => {
   return (
     <div
-      className={`h-full w-full min-w-[400px] ${backgroundImage} bg-cover bg-center bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
+      className={`h-full w-full min-w-[300px] ${backgroundImage} bg-cover bg-center bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
     >
       <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-8">
         <div className="flexCenter gap-4">
