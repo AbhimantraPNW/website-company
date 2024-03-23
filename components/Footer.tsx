fe442b9dd960343e27512b021e23e3f4e2aae724
AbhimantraPNW <abhimantrapnw04@gmail.com>
2024-03-23 07:42:40 +0700

    add link ig

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
                 <FooterColumn title={SOCIALS.title} >
                   <ul className="regular-14 flex gap-4 text-gray-30">
                     {SOCIALS.links.map((link) => (
-                      <Link href="/" key={link}>
+                      <Link href="https://www.instagram.com/paterapottery/" key={link}>
                         <Image src={link} alt="logo" width={24} height={24} />
                       </Link>
                     ))}
